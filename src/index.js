eb16dbb9f171a9c385656147cc98689ce0a576ff
YuriMendess <contato.yurimendess@gmail.com>
2022-03-31 19:14:26 -0300

    validate cpf done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,47 @@
 const express = require('express');
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
-const accounts = [];
+app.use(express.json());
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement []
+ */
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if(customerAlreadyExists) {
+    return response.status(400).json({ error:"Customer already exists!" });
+  }
+  
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: []
+  });
+
+  return response.status(201).send();
+});
+
+app.get("/statement/:cpf", (request, response) => {
+  const { cpf } = request.params;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  return response.json(customer.statement);
+});
 
 app.listen(3333);
 
-app.patch("/accounts", (request, response) => {
-  return response.json();
-})
