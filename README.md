8e8f12c42b53acf0f948a557f749407259e4490f
YuriMendess <contato.yurimendess@gmail.com>
2022-04-04 20:12:10 -0300

    middleware lesson done

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ### Requisitos
 
 - [x] Deve ser possível criar uma conta
-- [] Deve ser possível buscar o extrato bancário do cliente
+- [x] Deve ser possível buscar o extrato bancário do cliente
 - [] Deve ser possível realizar um depósito
 - [] Deve ser possível realizar um saque
 - [] Deve ser possível buscar o extrato bancário do cliente por data
@@ -13,8 +13,8 @@
 ## Regras de Negócio
 
 - [x] Deve ser possível cadastrar uma conta com CPF já existente
+- [x] Deve ser possível buscar extrato em uma conta não existente
 - [] Deve ser possível fazer depósito em uma conta não existente
-- [] Deve ser possível buscar extrato em uma conta não existente
 - [] Deve ser possível fazer saque em uma conta não existente
 - [] Deve ser possível excluir uma conta não existente
 - [] Deve ser possível fazer saque quando o saldo for insuficiente
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { request } = require('express');
 const express = require('express');
 const { v4: uuidv4 } = require("uuid");
 
@@ -12,6 +13,22 @@ app.use(express.json());
  */
 
 const customers = [];
+
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+  const customer = customers.find((customer) => customer.cpf === cpf);
+  
+  if(!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
+
+  request.customer = customer;
+  // Se existir um customer, a função irá dar um next
+  return next();
+}
+
+
 
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
@@ -34,15 +51,10 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement", (request, response) => {
-  const { cpf } = request.headers;
+// app.use(verifyIfExistsAccountCPF);
 
-  const customer = customers.find(customer => customer.cpf === cpf);
-
-  if(!customer) {
-    return response.status(400).json({ error: "Customer not found"});
-  }
-
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
   return response.json(customer.statement);
 });
 
